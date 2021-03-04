dba9968e6255927287408c4bb62db3fc2cbcdce0
Sophronios <frsophronios@outlook.com>
2021-03-04 17:20:16 -0500

    function to test if is latin square

--- scratch.c
+++ scratch.c
@@ -41,12 +41,41 @@ size_t exponent(size_t n)
   return temp;
 }
 
+/* Latin_Square switch_columns(Latin_Sqaure square,size_t col1, size_t col2) */
+/* { */
+/*   return */
+/* } */
+
+bool is_latin_square(Latin_Square *square)
+{
+  for (size_t to_match = 0;to_match < square->size; to_match++) {
+    for (size_t to_search = 0;to_search < square->size; to_search++) {
+      if ((square->array[to_match].row == square->array[to_search].row
+	   && square->array[to_match].elem == square->array[to_search].elem)
+	  &&
+	  (square->array[to_match].col == square->array[to_search].col
+	   && square->array[to_match].elem == square->array[to_search].elem))
+	continue;
+	  
+      if ((square->array[to_match].col == square->array[to_search].col
+	   && square->array[to_match].elem == square->array[to_search].elem)
+	  ||
+	  (square->array[to_match].row == square->array[to_search].row
+	   && square->array[to_match].elem == square->array[to_search].elem))
+	return false;
+      
+    }
+  }
+  
+  return true;
+}
+
 Latin_Square make_latin_square(String_View key)
 {
   Latin_Square square;
   square.key = key;
   square.size = key.count * key.count;
-  square.array = (Cell *)malloc(key.count * key.count * sizeof(Cell));
+  square.array = (Cell *)malloc(square.size * sizeof(Cell));
   assert(square.array != NULL);
   size_t r = -1;
   //  printf("%lu", square.key);
@@ -75,7 +104,13 @@ int main(int argc, char **argv)
   
   String_View key = SV("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
   Latin_Square a = make_latin_square(key);
+
   print_latin_square(&a);
+  
+  (is_latin_square(&a))
+    ? printf("This is a latin square.\n")
+    : printf("This is NOT a latin square.\n");
+  
   free(a.array);
   return 0;
 }
